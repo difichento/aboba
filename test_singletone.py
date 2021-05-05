e39c75b573341be767f97a03673b905169ea129b
polinavenberg <venberg1976@gmail.com>
2021-05-05 16:42:21 +0300

    added losted globals

--- test_singletone.py
+++ test_singletone.py
@@ -1,12 +1,11 @@
 from castle import Castle
 from Globals import Globals
+from path import PathFactory
 
-castle2 = Castle(50, 50, 200, Globals.castle_img)
+castle2 = Castle(PathFactory().make_path1(), 50, 50, 200, Globals.castle_img)
 print(castle2.health)
 print(castle2)
 
-castle3 = Castle(500, 500, 400, Globals.castle_img)
+castle3 = Castle(PathFactory().make_path3(), 500, 500, 400, Globals.castle_img)
 print(castle3.health)
 print(castle3)
-
-
